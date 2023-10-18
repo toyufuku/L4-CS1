a4f3682fc80357fed9024177bd03007ff17ac9f4
Tsuyoshi Toyofuku <2111140087w@ed.fuk.kindai.ac.jp>
2023-10-18 07:11:56 +0000

    hello

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'top/main'
+  post 'top/login'
+  root 'top#main'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
